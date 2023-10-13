7f95be726944a4255e51ae159b00e416dc654470
Arpit Gupta <guarpi@amazon.com>
2023-10-13 02:52:46 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,11 +1,4 @@
-module "s3_bucket" {
-  source = "terraform-aws-modules/s3-bucket/aws"
-
-  bucket = "arpit-guptat"
-  acl    = "private"
-
-  versioning = {
-    enabled = true
-  }
-
+module "s3-bucket" {
+  source  = "app.terraform.io/guarpi-aws/s3-bucket/aws"
+  version = "2.8.0"
 }
